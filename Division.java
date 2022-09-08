27501b1aefb3130c9f2aae494ad0d3123131b780
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-09-08 17:06:50 +0530

    import java.util.Scanner; class dic {     public static void main(String args[])     {         Scanner c= new Scanner(System.in);         int a,b,d;         a=c.nextInt();         b=c.nextInt();         d=a/b;         System.out.printf("%d",d);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class dic
+{
+    public static void main(String args[])
+    {
+        Scanner c= new Scanner(System.in);
+        int a,b,d;
+        a=c.nextInt();
+        b=c.nextInt();
+        d=a/b;
+        System.out.printf("%d",d);
+    }
+}
